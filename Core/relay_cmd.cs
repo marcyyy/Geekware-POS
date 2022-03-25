dc2ec7f9505fec53e2e8846bc04a570160da4cc3
Zyrah Avila <avila.zyrah@gmail.com>
2022-03-25 08:21:46 +0800

    Init files upload
    File Commit 1

---
+++ Core/relay_cmd.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace posproj.Core
+{
+	class relay_cmd : ICommand
+	{
+		private Action<object> _execute;
+		private Func<object, bool> _canExecute;
+
+		public event EventHandler CanExecuteChanged
+		{
+			add { CommandManager.RequerySuggested += value; }
+			remove { CommandManager.RequerySuggested -= value; }
+		}
+
+		public relay_cmd(Action<object> execute, Func<object, bool> canExecute = null)
+		{
+			_execute = execute;
+			_canExecute = canExecute;
+		}
+
+		public bool CanExecute(object parameter)
+		{
+			return _canExecute == null || _canExecute(parameter);
+		}
+
+		public void Execute(object parameter)
+		{
+			_execute(parameter);
+		}
+
+
+
+	}
+}
